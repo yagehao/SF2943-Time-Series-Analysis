b1a8430b9502b6d20c7db554a700d0c7483c4715
yage-hao <haoyage1998@gmail.com>
2021-05-06 04:35:58 +0200

    fit models

--- PART1/HAA.R
+++ PART1/HAA.R
@@ -1,44 +1,55 @@
-library(itsmr)
-data <- read.csv("~/KTH/Time series/GlobalTemperatures.csv")
+# import library
+library(ggplot2)
+library(forecast)
+library(dplyr)
+library(lubridate)
+#library(sarima)
+#library(itsmr)
 
-# Removing the na values and plots the uncertainty
-datalim = na.omit(datalim) 
-plot(datalim$LandAverageTemperatureUncertainty)
+# data preparation
+data <- read.csv("C:/Users/haoya/OneDrive/Desktop/SF2943/project/PART1/GlobalTemperatures.csv")
+datalim =data[1500:3180,1:3] # time 1874.12-2014.12
+datalim = na.omit(datalim)
+data_ts_raw = ts(data = datalim$LandAverageTemperature, start = decimal_date(ymd('1875-01-01'))) #, frequency = 12) # convert into time series
 
-#removes the first values since they have large uncertainty
-datalim =data[1500:3180,1:3]
-
-
-# Plot trend and seasonality
+# explanatory data analysis
+plot(data_ts_raw) # plot time series
+plot(datalim$LandAverageTemperatureUncertainty) # plot uncertainty
 temp = datalim$LandAverageTemperature
-plotc(trend(temp, 1))
-plotc(season(temp, 12))
+plotc(trend(temp, 1)) # plot trend
+plotc(season(temp, 12)) # plot seasonality
+
+# remove trend and seasonality
+data_ts1 = diff(data_ts_raw) # generate first difference of ts
+plot(data_ts1)
+data_ts2 = diff(data_ts_raw, lag = 12) # generate a diff of ts with lag=12
+plot(data_ts2)
+data_ts <- data_ts2
+
+# ACF and PACF
+layout(1:2)
+acf(data_ts, lag.max=50)
+pacf(data_ts, lag.max=50)
+
+# model fit and residual check
+# MA(12)
+model1 <- arima(data_ts, order = c(0,0,12)) 
+AIC(model1)
+checkresiduals(model1)
+# ARMA(12,12)
+model2 <- arima(data_ts, order = c(12,0,12)) 
+AIC(model2)
+checkresiduals(model2)
+# ARMA(1,12)
+model3 <- arima(data_ts, order = c(1,0,12)) 
+AIC(model3)
+checkresiduals(model3)
+# choose parameter p and q automatically
+automodel <- auto.arima(data_ts, stepwise=FALSE, parallel=TRUE, num.cores=4, approximation=FALSE)
+AIC(automodel)
+checkresiduals(automodel)
 
 
-# Remove the seasonality and trend
-temp_clean <- temp - season(temp, 12) - trend(temp, 1)
-plotc(temp_clean)
-
-# Acf and pacf
-acf(temp_clean)
-pacf(temp_clean)
 
 
-# different estimators
-#print(yw(temp_clean, p=1))
-#print(burg(temp_clean, p=1)) 
-#print(arma(temp_clean,p=12,q=12))
-
-# AR for 1-12 coeffecicents 
-
-error = rep(0, 12)
-for (i in 1:12) {
-  model3 = ar(temp_clean[0:1620],i)
-  
-  pred = predict(model3, n.ahead = 61)
-  error[i] <- sum(temp_clean[1621:1681] - pred$pred)
-  print(i)
-  
-}
-plot(1:12, error)
 
--- PART1/reference.md
+++ PART1/reference.md
@@ -14,3 +14,27 @@ https://www.zhihu.com/question/31833683
 
 https://stats.stackexchange.com/questions/241914/terms-cut-off-and-tail-off-about-acf-pacf-functions/241964
 
+**时间序列笔记-趋势与去趋势**
+
+https://www.jianshu.com/p/9711fafded15
+
+**R中的时间序列分析：ts（）函数中的频率值与acf图中的滞后**
+
+https://www.thinbug.com/q/23982656
+
+**根据ARIMA的ACF和PACF图得到相应的pq值**
+
+https://blog.csdn.net/Jasminexjf/article/details/94993362
+
+**如何根据自相关（ACF）图和偏自相关（PACF）图选择ARIMA模型的p、q值**
+
+https://blog.csdn.net/weixin_41013322/article/details/108801516?utm_medium=distribute.pc_relevant.none-task-blog-2%7Edefault%7EBlogCommendFromBaidu%7Edefault-5.control&depth_1-utm_source=distribute.pc_relevant.none-task-blog-2%7Edefault%7EBlogCommendFromBaidu%7Edefault-5.control
+
+**在R中建立ARIMA模型**
+
+https://otexts.com/fppcn/arima-r.html
+
+
+
+
+
